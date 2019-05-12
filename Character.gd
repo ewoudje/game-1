ec41a96635a63b537436e62eb5e642230394dafe
ewoudje <ewoud.dierickx@hotmail.be>
2019-05-12 17:47:07 +0200

    Character Impl

---
+++ Character.gd
@@ -0,0 +1,40 @@
+extends KinematicBody2D
+
+export (int) var speed = 200;
+
+var velocity = Vector2();
+
+func get_input():
+    velocity = Vector2()
+    if Input.is_action_pressed('right'):
+        velocity.x += 1;
+    if Input.is_action_pressed('left'):
+        velocity.x -= 1;
+    if Input.is_action_pressed('bottom'):
+        velocity.y += 1;
+    if Input.is_action_pressed('top'):
+        velocity.y -= 1;
+    velocity = velocity.normalized() * speed;
+
+func _physics_process(delta):
+    get_input();
+    move_and_slide(velocity);
+
+export (int) var level = 0;
+
+func update_mask():
+	collision_mask = level ^ 0x3FF;
+
+var entered = []; 
+
+func _on_body_entered(body):
+	entered.push_front(body.collision_layer);
+
+
+func _on_body_exited(body):
+	entered.remove(entered.find(body.collision_layer));
+	var fall = 0;
+	for e in entered:
+		fall = max(e, fall);
+	level = fall;
+	update_mask();
